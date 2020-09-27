2d3c7478ba31e3ff0f2790b84561b86b4dd36144
Juno <juno.burger@pm.me>
2020-09-27 19:34:58 +0200

    Added while loops

--- Chips.cpp
+++ Chips.cpp
@@ -3,45 +3,90 @@
 #include <cstdlib>
 using namespace std;
 
-const int MAX_CHIPS = 100;
 const float MAX_TURN = .5;
+const int MAX_CHIPS = 100;
 
 int main()
 {
     bool player1Turn = true;
     bool gameOver = false;
-    int chipsLeftInPile = 0;
+    int chipsInPile = 0;
     int chipsTaken = 0;
     int maxPerTurn = 0;
     string playerNames[2];
+    char userChoice;
 
     //seeding the number generator
     srand(time(0));
 
     cout << "Player 1, please enter your name: \n";
     cin >> playerNames[0];
-    cout << "Player 2, please enter your name: \n";
+    cout << "\n Thanks and good luck!\n"
+         << endl
+         << " Player 2, please enter your name: \n";
     cin >> playerNames[1];
-    cout << "Thanks and good luck!\n";
-    cout << playerNames[0] << " & " << playerNames[1] << " are ready to play!" << endl;
+    cout << "\n Thanks and good luck!" << playerNames[0] << " & " << playerNames[1] << " are ready to play!" << endl;
 
     //start the game with a random number of chips in the pile
-    chipsLeftInPile = (rand() % MAX_CHIPS) + 1;
-    cout << "This round starts with " << chipsLeftInPile << " chips in the pile\n";
-    if (player1Turn)
+    do
     {
-        cout << playerNames[0] << " How many chips would you like? \n";
-    }
-    else
-    {
-        cout << playerNames[1] << " How many chips would you like?\n";
-    }
-    maxPerTurn = (MAX_TURN * chipsLeftInPile);
-    cout << "You can  take up to  " << maxPerTurn << " chips" << endl;
-    cin >> chipsTaken;
-    chipsTaken = (rand() % maxPerTurn) + 1;
 
-    cout << "Number taken: " << chipsTaken << endl;
+        chipsInPile = (rand() % MAX_CHIPS) + 1;
+        cout << "This round will start with " << chipsInPile << " chips in the pile\n";
+        gameOver = false;
+        while (gameOver == false)
+        {
+
+            do
+            {
+                maxPerTurn = (MAX_TURN * chipsInPile);
+                if (player1Turn)
+                {
+                    cout << playerNames[0] << " How many chips would  you like?\n";
+                }
+                else
+                {
+                    cout << playerNames[1] << " How many chips would  you like?\n";
+                }
+                cout << "You can take up to ";
+                if (maxPerTurn == 0)
+                {
+                    cout << " 1\n";
+                }
+                else
+                {
+                    cout << maxPerTurn << endl;
+                }
+                cin >> chipsTaken;
+
+                //commented out solution portion from Chapter 2
+                //chipsTaken = (rand() % maxPerTurn) + 1;
+
+            } while ((chipsTaken > maxPerTurn) && (chipsInPile > 1));
+
+            chipsInPile = chipsInPile - chipsTaken;
+            cout << "There are " << chipsInPile << " left in the pile\n";
+            if (chipsInPile == 0)
+            {
+                gameOver = true;
+                if (player1Turn)
+                {
+                    cout << playerNames[1] << ", congratulations you won\n";
+                }
+                else
+                {
+                    cout << playerNames[0] << ", congratulations you won\n";
+                }
+            }
+            else
+            {
+                player1Turn = !player1Turn;
+            }
+        }
+        cout << "Do you wish to play again? (Y/N)\n";
+        cin >> userChoice;
+
+    } while ((userChoice == 'y') || (userChoice == 'Y'));
 
     return 0;
 }
